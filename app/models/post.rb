0ccfa3fd1c5779c91549b8e57102b1bdec4449a2
Evan Farrell <evandev@gastrograph.com>
2014-10-02 14:19:54 -0400

    subtitle to thing

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,4 @@
 class Post < ActiveRecord::Base
-  attr_accessible :content, :markdown, :title, :post_date, :tags, :description, :author
-  validates :title, :author, :content, presence: true
+  attr_accessible :content, :markdown, :title, :post_date, :tags, :description, :author, :subtitle
+  validates :title, :author, :content, :subtitle, :description, :tags, :post_date, presence: true
 end
