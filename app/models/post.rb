8bbf1af15e89e26ba6eafa17cc030c2098de78b2
Evan Farrell <evandev@gastrograph.com>
2015-02-26 10:58:37 -0500

    Ready for github

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,7 @@
 class Post < ActiveRecord::Base
   attr_accessible :content, :markdown, :title, :post_date, :tags, :description, :author, :subtitle
   validates :title, :author, :content, :description, :tags, :post_date, presence: true
+  before_save do |post|
+      DateTime.strptime(post.post_date, "%F %T")
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20141002161941) do
+ActiveRecord::Schema.define(:version => 20141002213542) do
 
   create_table "posts", :force => true do |t|
     t.string   "title"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(:version => 20141002161941) do
     t.string   "post_date"
     t.string   "description"
     t.string   "tags"
+    t.string   "subtitle"
   end
 
 end
